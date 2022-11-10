2802c819c40be6507cd119a4d4aec4c9c9cf437b
TheGamer3514 <thegamer3514@gmail.com>
2022-11-10 17:03:09 +0000

    Update Info.py

--- cogs/Info.py
+++ cogs/Info.py
@@ -10,15 +10,15 @@ class Info(commands.Cog):
         await ctx.send("Servers: 1\nUptime: 99.99%\nBot Made By Gamer3514. Coming Soon!")
     @commands.command()
     async def info(self, ctx):
-        embed=discord.Embed(title="Bot Info", url="https://github.com/TheGamer3514/", description="**Info:**\n``Bot Creator:`` Gamer3514#7679\n``Host:`` [Huguitis Nodes](https://discord.gg/byarHVDznN)\n``Support Server:`` [3514](https://discord.gg/WeQ3TpdfZM)", color=0xFCBA03)
+        embed=discord.Embed(title="Bot Info", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="**Info:**\n``Bot Creator:`` Gamer3514#7679\n``Host:`` [Huguitis Nodes](https://discord.gg/byarHVDznN)\n``Support Server:`` [3514](https://discord.gg/WeQ3TpdfZM)", color=0xFCBA03)
         await ctx.send(embed=embed)
     @commands.command()
     async def botinfo(self, ctx):
-        embed=discord.Embed(title="Bot Info", url="https://github.com/TheGamer3514/", description="**Info:**\n``Bot Creator:`` Gamer3514#7679\n``Host:`` [Huguitis Nodes](https://discord.gg/byarHVDznN)\n``Support Server:`` [3514](https://discord.gg/WeQ3TpdfZM)", color=0xFCBA03)
+        embed=discord.Embed(title="Bot Info", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="**Info:**\n``Bot Creator:`` Gamer3514#7679\n``Host:`` [Huguitis Nodes](https://discord.gg/byarHVDznN)\n``Support Server:`` [3514](https://discord.gg/WeQ3TpdfZM)", color=0xFCBA03)
         await ctx.send(embed=embed)
     @commands.command()
     async def help(self, ctx):
-        embed=discord.Embed(title="Bot Help", url="https://github.com/TheGamer3514/", description="**Info:**\n``!help`` - Shows The Help Embed\n``!botinfo`` - Shows The Bot Info\n``!stats`` - Shows The Bot Stats", color=0xFCBA03)
+        embed=discord.Embed(title="Bot Help", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="**Info:**\n``!help`` - Shows The Help Embed\n``!botinfo`` - Shows The Bot Info\n``!stats`` - Shows The Bot Stats", color=0xFCBA03)
         await ctx.send(embed=embed)
 async def setup(client):
     await client.add_cog(Info(client))
