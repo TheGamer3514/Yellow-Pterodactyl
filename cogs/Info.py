c86c814b3a15bb023e7bd78338bd13f7f26bb883
TheGamer3514 <gamer@3514.ga>
2022-11-11 16:55:37 +0000

    Update Info.py

--- cogs/Info.py
+++ cogs/Info.py
@@ -8,17 +8,21 @@ class Info(commands.Cog):
     @commands.command()
     async def stats(self, ctx):
         await ctx.send("Servers: 1\nUptime: 99.99%\nBot Made By Gamer3514. Coming Soon!")
-    @commands.command()
+    @commands.command(aliases=['botinfo'])
     async def info(self, ctx):
         embed=discord.Embed(title="Bot Info", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="**Info:**\n``Bot Creator:`` Gamer3514#7679\n``Support Server:`` [3514](https://discord.gg/WeQ3TpdfZM)", color=0xFCBA03)
         await ctx.send(embed=embed)
-    @commands.command()
-    async def botinfo(self, ctx):
-        embed=discord.Embed(title="Bot Info", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="**Info:**\n``Bot Creator:`` Gamer3514#7679\n``Support Server:`` [3514](https://discord.gg/WeQ3TpdfZM)", color=0xFCBA03)
-        await ctx.send(embed=embed)
-    @commands.command()
+    @commands.command(aliases=['bothelp'])
     async def help(self, ctx):
-        embed=discord.Embed(title="Bot Help", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="**Info:**\n``!help`` - Shows The Help Embed\n``!botinfo`` - Shows The Bot Info\n``!stats`` - Shows The Bot Stats", color=0xFCBA03)
+        embed=discord.Embed(title="Bot Help", url="https://github.com/TheGamer3514/Yellow-Pterodactyl, description="**Info:**\n``!help`` - Shows The Help Embed\n``!botinfo`` - Shows The Bot Info\n``!credits`` - Shows The Bot Credits\n``!stats`` - Shows The Bot Stats\n``!update`` - Shows The Bot Update Logs\n**Fun:**\n``!magic8ball`` - Ask the magic 8ball a question or two!\n**Utility:**\n``!avatar`` - Get a user's avatar!", color=0xFCBA03)
+        await ctx.send(embed=embed)
+    @commands.command(aliases=['botcredits'])
+    async def credits(self, ctx):
+        embed=discord.Embed(title="Credits", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="``Bot Creator:`` Gamer3514#7679\n``Github`` - [Link](https://github.com/TheGamer3514/Yellow-Pterodactyl)\n``Support Server`` - [Link](https://discord.gg/WeQ3TpdfZM)", color=0xFCBA03)
+        await ctx.send(embed=embed)
+    @commands.command(aliases=['updatelog'])
+    async def update(self, ctx):
+        embed=discord.Embed(title="Update Logs | v0.0.1", url="https://github.com/TheGamer3514/Yellow-Pterodactyl", description="First Release! Epic!\n**Stuff Added:**\n All the commands\nbot avatar\nAdded To Some Botlists! !botlists to virw them all!", color=0xFCBA03)
         await ctx.send(embed=embed)
 async def setup(client):
     await client.add_cog(Info(client))
