c930ae443189242818ca6efaa5ccf5acea6c1c05
TheGamer3514 <thegamer3514@gmail.com>
2023-04-06 19:53:32 +0100

    stopped using cogs, new stats & purge commands

--- main.py
+++ main.py
@@ -1,40 +1,99 @@
-# import modules
+#import modules
 import discord
 import os
 import json
 import asyncio
-from discord.ext import commands
-#logging
+import psutil
+from discord_webhook import DiscordWebhook
+import random
+import multiprocessing
 import logging
-
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-
+from discord.ext import commands
+ball8answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes Signs point to yes', 'Reply hazy', 'try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+handler = logging.FileHandler(filename='errors.log', encoding='utf-8', mode='w')
 #load config
 with open('./config.json') as f:
   data = json.load(f)
   for c in data['botConfig']:
-        print('Online! Ready To Be Controlled')
         print('Prefix: ' + c['prefix'])
-#define client
-bot = commands.Bot(command_prefix = c['prefix'],intents=discord.Intents.all(),status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="Coming Soon!"))
-bot.remove_command('help')
-#on guild join
+#Define Bot
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.AutoShardedBot(command_prefix = c['prefix'],intents=intents,status=discord.Status.idle,case_insensitive=True)
+#bot.remove_command('help')
+#------------------------------------------Bot Events-------------------------------------------------
+#Auto Updating Status
+async def StatusChange():
+    while True:
+        usercount = len(list(filter(lambda m: m.bot == False, bot.users)))
+        await bot.change_presence(activity=discord.Game(f"{c['prefix']}help | {len(bot.guilds)} Servers"))
+        await asyncio.sleep(35)
+        await bot.change_presence(activity=discord.Game(f"{c['prefix']}help | {usercount} Users"))
+        await asyncio.sleep(35)
+        await bot.change_presence(activity=discord.Game(f"{c['prefix']}help | discord.gg/3qvpkgWSbF"))
+        await asyncio.sleep(35)
+        await bot.change_presence(activity=discord.Game(f"{c['prefix']}help | Gamer3514#7679"))
+        await asyncio.sleep(35)
+#Webhook Logging
+async def Webhooklogging(channel,message):
+        webhook = DiscordWebhook(url=channel, content=message)
+        response = webhook.execute()
+        print(message)
+#Event that runs when bot is online
 @bot.event
-async def on_guild_join(guild):
-    for channel in guild.text_channels:
-        if channel.permissions_for(guild.me).send_messages:
-            embed = discord.Embed(title="Yellow Pterodactyl", description="<a:arrow:942025249908752395>Hello. Thank you for inviting me to your server! Get started by running ``!help``!", color=(65535))
-            embed.add_field(name="<:no:942044962541936671> WARNING <:no:942044962541936671>", value='<a:arrow:942025249908752395>This bot is still in active development! Downtime May Occur Often!', inline=False)
-            await channel.send(embed=embed)
-        break
-#start bot
-async def load():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            await bot.load_extension(f"cogs.{filename[:-3]}")
- #start bot
-async def main():
-    async with bot:
-        await load()
-        await bot.start(c['token'])
-asyncio.run(main())
+async def on_ready():
+    print(f'{bot.user} Is Now Online!')
+    await bot.loop.create_task(StatusChange())
+#------------------------------------------Prefix Commands-------------------------------------------------
+#--------------------------------Info---------------------------------------
+#Stats Command
+@bot.command(name='stats', aliases=['botstats'])
+async def stats(ctx):
+    usercount = len(list(filter(lambda m: m.bot == False, bot.users)))
+    await Webhooklogging(c['webhookurl'], f"{ctx.author} | {ctx.guild.id} -> {c['prefix']}stats")
+    embed = discord.Embed(
+        title = f'My Stats:',
+        colour=discord.Colour.random())
+    embed.add_field(name="Bot Owner", value="<@763471049894527006> (763471049894527006) Gamer3514#7679", inline=False)
+    embed.add_field(name="Cpu Cores", value=multiprocessing.cpu_count(), inline=False)
+    embed.add_field(name="Server Count", value=f"{len(bot.guilds)}", inline=False)
+    embed.add_field(name="User Count", value=f"{usercount}", inline=False)
+    embed.add_field(name="Bot Latency", value=f"{round(bot.latency * 1000)}ms", inline=False)
+    embed.add_field(name="Cpu Usage", value=f"{psutil.cpu_percent()}%", inline=False)
+    embed.add_field(name="Memory Usage", value=f"{psutil.virtual_memory().percent}%", inline=False)
+    embed.add_field(name="Total Memory", value=f"{round(psutil.virtual_memory().total / (1024.0 **3))} GB", inline=False)
+    await ctx.send(embed=embed)
+#--------------------------------Fun---------------------------------------
+#8Ball Command
+@bot.command(aliases=['8ball'])
+async def magic8ball(ctx):
+    await Webhooklogging(c['webhookurl'], f"{ctx.author} | {ctx.guild.id} -> {c['prefix']}8ball")
+    embed=discord.Embed(title=":8ball: Magic 8 Ball :8ball:", description=ball8answers[random.randint(0, len(ball8answers)-1)], colour=discord.Colour.random())
+    await ctx.send(embed=embed)
+#--------------------------------Utility---------------------------------------
+#Purge Command
+@bot.command(aliases=['useravatar'])
+async def avatar(ctx,  member: discord.Member = None):
+    if not member:
+        member = ctx.author
+    await Webhooklogging(c['webhookurl'], f"{ctx.author} | {ctx.guild.id} -> {c['prefix']}avatar {member}")
+    embed = discord.Embed(colour=discord.Colour.random(), title=f"{member}'s avatar")
+    embed.set_image(url=member.avatar.url)
+    await ctx.send(embed=embed)
+#--------------------------------Moderation---------------------------------------
+#Avatar Command
+@bot.command(aliases=['purgemessages'])
+async def purge(ctx, amount: int = None):
+    await Webhooklogging(c['webhookurl'], f"{ctx.author} | {ctx.guild.id} -> {c['prefix']}purge {amount}")
+    if not amount:
+        embed = discord.Embed(colour=discord.Colour.red(), title=f"No Purge Amount Given!\nCorrect Usage: {c['prefix']}purge [amount]")
+    try:
+        embed = discord.Embed(colour=discord.Colour.random(), title=f"Purged {amount} Messages!")
+        await ctx.channel.purge(limit=amount)
+    except:
+        embed = discord.Embed(colour=discord.Colour.red(), title=f"Failed to purge {amount} messages.\nThis could be due to the messages being older than 14 days or because i am missing ``manage messages`` permission.")
+    message = await ctx.send(embed=embed)
+    await asyncio.sleep(5)
+    message.delete
+bot.run(c['token'], log_handler=handler, log_level=logging.ERROR)
